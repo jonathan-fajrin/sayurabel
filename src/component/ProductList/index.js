9b79b24def5dfe16b6919bf46b52a1f404e5ed44
ajinbramantio <ajinbramantio@gmail.com>
2019-02-16 14:33:24 +0700

    Add dummy data to productList

--- src/component/ProductList/index.js
+++ src/component/ProductList/index.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
 
 class ProductList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: 'tomato',
+          image: '/assets/tomato.jpg',
+          price: 12000
+        },
+        {
+          id: 2,
+          name: 'potato',
+          image: '/assets/potato.png',
+          price: 12000
+        },
+        {
+          id: 3,
+          name: 'orange',
+          image: '/assets/orange.jpg',
+          price: 12000
+        }
+      ]
+    }
+  }
   render() {
+    console.log(this.state.data)
+
     return (
-      <div>
-        <p>Product List</p>
-      </div>
+      <ul>
+        {this.state.data.map((product, index) => {
+          return (
+            <li key={index}>
+              <div>
+                <img src={product.image} />
+              </div>
+              <div>
+                <p>{product.name}</p>
+              </div>
+              <div>
+                <p>{product.price}</p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
