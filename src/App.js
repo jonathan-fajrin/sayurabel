9789e7975466496512618037b1d29d7f32275f75
ajinbramantio <ajinbramantio@gmail.com>
2019-02-16 13:25:03 +0700

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import Header from './component/Header'
 
 class App extends Component {
   render() {
-    return <div>Sayurabel</div>
+    return (
+      <div>
+        <Header />
+      </div>
+    )
   }
 }
 
