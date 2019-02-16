6462ee14aa63afd1b1abfc69cbb0784c8077cb37
ajinbramantio <ajinbramantio@gmail.com>
2019-02-16 13:39:38 +0700

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './component/Header'
 import Content from './component/Content'
+import Footer from './component/Footer'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Header />
         <Content />
+        <Footer />
       </div>
     )
   }
---
+++ src/component/Footer/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return <footer> ajin and jonathan | 2019</footer>
+  }
+}
+
+export default Footer
