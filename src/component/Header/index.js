4130d308626425be9bb51abfc9fb0c6ae08b64b5
ajinbramantio <ajinbramantio@gmail.com>
2019-02-16 15:08:06 +0700

    Add style to Header

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  background: green;
+`
+const StyledIcon = styled.img`
+  width: 30px;
+  height: 20px;
+`
+
+const StyledContainer = styled.div`
+  background: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+  width: 500px;
+  height: 80px;
+`
 
 class Header extends Component {
   render() {
     return (
-      <header>
-        <h1>Sayurabel</h1>
-      </header>
+      <StyledHeader>
+        <StyledContainer>
+          <div>Sayurabel</div>
+          <StyledIcon src="/assets/cart.png" />
+        </StyledContainer>
+      </StyledHeader>
     )
   }
 }
